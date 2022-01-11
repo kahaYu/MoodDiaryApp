f6331f14c32417dfb331596f19946583e096afc5
Yura <yurikolesnicov@gmail.com>
2022-01-11 13:37:47 +0300

    App is ready

--- app/src/main/java/com/yurakolesnikov/mooddiary/ui/NavBarFragment.kt
+++ app/src/main/java/com/yurakolesnikov/mooddiary/ui/NavBarFragment.kt
@@ -7,8 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
-import com.yurakolesnikov.mooddiary.database.model.Note
 import com.yurakolesnikov.mooddiary.databinding.FragmentNavBarBinding
 import com.yurakolesnikov.mooddiary.ui.mainActivity.MainActivityViewModel
 import com.yurakolesnikov.mooddiary.utils.AutoClearedValue
@@ -23,8 +21,6 @@ class NavBarFragment : Fragment() {
 
     private val vm: MainActivityViewModel by activityViewModels()
 
-    private lateinit var lastNote: Note
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,45 +33,41 @@ class NavBarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Need to remove bg here, because if to set transparent color in XML, shadow remains.
+        removeBackground(binding) // Remove bg here, cause if to set transparent color in XML, shadow remains.
+
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        binding.vm = vm
+    }
+
+    fun onAddPressed() {
+        val currentDate = getCurrentDateTime().toString("dd.MM.yyyy")
+        val lastDate = if (vm.notesNoLd.isEmpty()) "00.00.0000" else vm.notesNoLd.last().date
+        if (lastDate != currentDate || vm.isAlwaysYes) { // Show add dialog if now is new day or user
+            AddNoteFragment().show(parentFragmentManager, "AddNoteFragment") // Choose remember always YES
+        } else if (vm.notesNoLd.last().date == currentDate && vm.isAlwaysNo) {
+            Toast.makeText(requireContext(), "Try again tomorrow", Toast.LENGTH_SHORT).show()
+        } else
+            ConfirmationFragment().show(parentFragmentManager, "Conf fragment")
+    }
+
+    fun onDeleteAllPressed() {
+        vm.deleteAllNotes()
+    }
+
+    fun onSortPressed() {
+        SortFragment().show(parentFragmentManager, "Sort fragment") // Show sort dialog
+    }
+
+    fun onFilterPressed() {
+        FilterFragment().show(parentFragmentManager, "Filter fragment") // Show filter dialog
+    }
+
+    private fun removeBackground(binding: FragmentNavBarBinding) {
         binding.apply {
             buttonDeleteAll.background = null
             buttonSort.background = null
             buttonFilter.background = null
         }
-
-        binding.fragment = this
-        binding.lifecycleOwner = this
-        binding.vm = vm
-
-        vm.getAllNotes().observe(viewLifecycleOwner, Observer { notes ->
-            if (notes.size > 0) {
-                lastNote = notes.last()
-            } else lastNote = Note("00.00.0000", 1)
-        })
-
     }
-
-    fun onAddPressed () {
-        val sdf = getCurrentDateTime().toString("dd.MM.yyyy")
-         if (lastNote.date != sdf || vm.isAlwaysYes) {
-             AddNoteFragment().show(parentFragmentManager, "123")
-        } else if (lastNote.date == sdf && vm.isAlwaysNo) {
-            Toast.makeText(requireContext(), "Try again tomorrow", Toast.LENGTH_SHORT).show()
-         } else
-             ConfirmationFragment().show(parentFragmentManager, "Conf fragment")
-    }
-
-    fun onDeleteAllPressed () {
-        vm.deleteAllNotes()
-    }
-
-    fun onSortPressed () {
-        SortFragment().show(parentFragmentManager, "Sort fragment")
-    }
-
-    fun onFilterPressed () {
-        FilterFragment().show(parentFragmentManager, "Sort fragment")
-    }
-
 }
